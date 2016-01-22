6618c7fc54e98b3acaa09b2d36b6fb2c3ed7e1aa
Andreas <bloxxer9001@gmail.com>
2016-01-22 19:01:50 +0100

    Finished project. ;) Set the variables in the start of the program.

--- TS3PokeBot/src/main/Main.java
+++ TS3PokeBot/src/main/Main.java
@@ -11,55 +11,84 @@ import com.github.theholywaffle.teamspeak3.api.event.TS3EventAdapter;
 import com.github.theholywaffle.teamspeak3.api.event.TS3EventType;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Channel;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
 
 public class Main {
 	
+	// Variable declaration start
+	
+	private final static String HOST		= "46.32.39.140";		// The server IP we connect to
+	private final static int PORT			= 9002;					// The server port we connect to
+	private final static String BOTNAME		= "Poker";				// The username for our bot. If it is in use, we start adding number 1, 2, ... at the end
+	private final static String POKEMSG		= "You got poked.";		// The message to be poked
+	private static int TARGETID				= 0;					// Our target client's ID. Does this even change? Use TARGETNAME if you do not know the ID.
+	private final static String TARGETNAME	= "Poor guy";			// Setting this variable to anything other than null will update TARGETID to the desired ID if found.
+	private static int POKEAMOUNT			= 5;					// Extra: Let the bot poke MULTIPLE times! Wow!
+	private static int POKEINTERVAL			= 2000;					// Extra: ^ Poking multiple times comes in intervals!
+	
+	private final static int CHANNELID		= 2;					// Make the bot join a channel, if the value is not 0.
+	
+	// Variable declaration end
+	
 	public static void main(String[] args) {
 		
+		// Connect to the server.
 		final TS3Config config = new TS3Config();
-		config.setHost("46.32.39.140");
-		config.setDebugLevel(Level.ALL);
+		config.setHost(HOST);
+		config.setDebugLevel(Level.OFF);
 
 		final TS3Query query = new TS3Query(config);
 		query.connect();
 
 		final TS3Api api = query.getApi();
-		api.login("", "-");
-		List<Channel> channels = api.getChannels();
+		api.selectVirtualServerByPort(PORT);
+		
+		/*List<Channel> channels = api.getChannels();
 		for (Channel c : channels) {
-			System.out.println("Channel found: " + c.getName() + " ; " + c.getId());
+			System.out.println("Channel found: Name = " + c.getName() + " ; ID = " + c.getId() + " ; TotalClients = " + c.getTotalClients());
+		}*/
+		
+		boolean successBotname = api.setNickname(BOTNAME);
+		if (!successBotname) {
+			attemptSetNickname(api, 1);
 		}
-		api.selectVirtualServerById(1);
-		api.setNickname("PutPut");
-		api.sendChannelMessage("PutPut is online!");
-
-		// Get our own client ID by running the "whoami" command
-		final int clientId = api.whoAmI().getId();
-
-		// Listen to chat in the channel the query is currently in
-		// As we never changed the channel, this will be the default channel of the server
-		api.registerEvent(TS3EventType.TEXT_CHANNEL, 0);
-
-		// Register the event listener
-		api.addTS3Listeners(new TS3EventAdapter() {
-
-			@Override
-			public void onTextMessage(TextMessageEvent e) {
-				// Only react to channel messages not sent by the query itself
-				if (e.getTargetMode() == TextMessageTargetMode.CHANNEL && e.getInvokerId() != clientId) {
-					String message = e.getMessage().toLowerCase();
-
-					if (message.equals("!ping")) {
-						// Answer "!ping" with "pong"
-						api.sendChannelMessage("pong");
-					} else if (message.startsWith("hello")) {
-						// Greet whoever said hello
-						// Message: "Hello <client name>!"
-						api.sendChannelMessage("Hello " + e.getInvokerName() + "!");
+		
+		// Look for the target by name, not ID.
+		if (TARGETNAME != null) {
+			Client target = api.getClientByNameExact(TARGETNAME, true);
+			if (target != null) {
+				TARGETID = target.getId();
+			}
+		}
+		
+		// Join the channel, if possible
+		if (CHANNELID != 0) {
+			api.selectVirtualServerById(CHANNELID);
+		}
+		
+		// POKE POKE POKE! If the amount of pokes is >1, we loop. Otherwise, we just poke.
+		if (POKEAMOUNT > 1) {
+			for (int i = 0; i < POKEAMOUNT; i++) {
+				api.pokeClient(TARGETID, POKEMSG);
+				if (i < POKEAMOUNT - 1) { // Do not wait if this was the last poke.
+					try {
+						Thread.sleep(POKEINTERVAL);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
 					}
 				}
 			}
-		});
+		} else {
+			api.pokeClient(TARGETID, POKEMSG);
+		}
+	}
+	
+	// Internal method for finding an available nickname.
+	private static void attemptSetNickname(TS3Api api, int num) {
+		boolean success = api.setNickname(BOTNAME + num);
+		if (!success) {
+			attemptSetNickname(api, num + 1);
+		}
 	}
 	
 }
